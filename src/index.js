0eec99f43bc761fc89718a4ca3026c0ac80684bb
ashwin74 <ashwin7411@gmail.com>
2019-07-07 00:14:52 +0530

    seasons

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Loader from './Loader';
+
+class App extends React.Component {
+    
+    state = {lat: null, errorMessage: ''};
+
+
+    componentDidMount() {
+        
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ lat:position.coords.latitude});
+            },
+            (err) => {
+                this.setState({errorMessage: err.message});
+            }
+        );
+
+    }
+
+
+    renderContent() {
+        if(this.state.errorMessage && !this.state.lat) {
+            
+        }
+
+        if(this.state.lat && !this.state.errorMessage) {
+            return <SeasonDisplay lat={this.state.lat}/>;
+        }
+
+        return <Loader message="Waiting for Location"/>;
+    };
+
+    //React says we have to define render! if not defined, react throws an error.
+    render() {
+        return (
+            <div>
+                {this.renderContent()}
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
